4626078f9c588ad260f7a89894e54d6b49403bd2
Sofa <sofo4kavakula@gmail.com>
2026-01-14 17:44:26 +0200

    feat(user): implement CreateUserView and ManageUserView

--- user/views.py
+++ user/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Register a new user (POST: users/)"""
+
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Current user profile management (GET/PUT/PATCH: users/me/)"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        """Returns the currently authenticated user"""
+        return self.request.user
