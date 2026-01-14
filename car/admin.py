8faf3e8fbe19f90354c0393e6bb1d5208d69dd94
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-01-14 09:50:57 +0200

    feat(car): add car model with basic admin panel

--- car/admin.py
+++ car/admin.py
@@ -1,2 +1,8 @@
+from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("brand", "model", "year", "daily_rate", "inventory")
+    list_filter = ("brand", "fuel_type")
--- car/models.py
+++ car/models.py
@@ -1,2 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Car(models.Model):
+    class FuelType(models.TextChoices):
+        GAS = "GAS", _("Gas")
+        DIESEL = "DIESEL", _("Diesel")
+        HYBRID = "HYBRID", _("Hybrid")
+        ELECTRIC = "ELECTRIC", _("Electric")
+
+    brand = models.CharField(max_length=63)
+    model = models.CharField(max_length=63)
+    year = models.IntegerField()
+    fuel_type = models.CharField(max_length=10, choices=FuelType.choices)
+    daily_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["brand", "model"]
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
