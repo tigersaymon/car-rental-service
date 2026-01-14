3ff3cc0b634aeb6575b6c9d965cd520b9de8826e
Sofa <sofo4kavakula@gmail.com>
2026-01-15 00:26:12 +0200

    style: fix import sorting with ruff

--- user/tests/test_admin.py
+++ user/tests/test_admin.py
@@ -1,6 +1,6 @@
-from django.test import TestCase, Client
-from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
 
 
 class AdminSiteTests(TestCase):
