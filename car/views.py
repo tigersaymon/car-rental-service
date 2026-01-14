43013acf6c4dab3855984b98e5e7f53b8a8e34df
Oleksii Shcherbak <qksardas001@mail.com>
2026-01-15 00:15:54 +0200

    feat(car): implement CarViewSet with CRUD and role-based permissions

--- car/views.py
+++ car/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from .models import Car
+from .serializers import CarSerializer
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [IsAuthenticated()]
+        return [IsAdminUser()]
