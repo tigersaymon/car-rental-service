d6206cbca74db5a0f3fdfaf9e7bbbc64bb1eae57
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-01-14 09:25:44 +0200

    feat(users): implement custom user model with email auth

--- user/models.py
+++ user/models.py
@@ -1,2 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from user.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.email
