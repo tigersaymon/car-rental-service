ed5d5e11be8725c5414179312ee815396aa80568
Oleksii Shcherbak <qksardas001@mail.com>
2026-01-15 21:39:04 +0200

    refactor(car): rename year_from/year_to filters to min_year/max_year

--- car/filters.py
+++ car/filters.py
@@ -7,8 +7,8 @@ class CarFilter(django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="daily_rate", lookup_expr="gte")
     price_max = django_filters.NumberFilter(field_name="daily_rate", lookup_expr="lte")
 
-    year_from = django_filters.NumberFilter(field_name="year", lookup_expr="gte")
-    year_to = django_filters.NumberFilter(field_name="year", lookup_expr="lte")
+    min_year = django_filters.NumberFilter(field_name="year", lookup_expr="gte")
+    max_year = django_filters.NumberFilter(field_name="year", lookup_expr="lte")
 
     available = django_filters.BooleanFilter(method="filter_available", label="Available for rent")
 
