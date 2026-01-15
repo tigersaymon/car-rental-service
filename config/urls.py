ca458153fe4c17cf66ce41446865b001f61ed886
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-01-15 23:46:30 +0200

    refactor: restructure URL routing for cleaner namespace usage
    # Conflicts:
    #	config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
-    path("api/", include("car.urls", namespace="car")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/doc/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path("api/", include("rental.urls", namespace="rental")),
+    path("api/cars/", include("car.urls", namespace="car")),
+    path("api/rentals/", include("rental.urls", namespace="rental")),
 ]
