6bd65067b9df7672ff98bb6d61b07c715860052a
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-01-16 11:25:25 +0200

    Add ngrok runner for django development

---
+++ payment/run_with_ngrok.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+from pyngrok import ngrok, conf
+
+if __name__ == "__main__":
+    auth_token = os.getenv("NGROK_AUTH")
+    if auth_token:
+        conf.get_default().auth_token = auth_token
+
+    public_url = ngrok.connect(8000)
+    print("Ngrok public URL:", public_url)
+
+    subprocess.call(["python", "manage.py", "runserver", "0.0.0.0:8000"])
