93d8f7175f0ad46fa6ef6e6f9e8343135b2ef589
Vadim <vadimchubok@gmail.com>
2026-01-18 13:49:24 +0200

    feat(auth): added required libraries and settings for google auth service

--- config/settings.py
+++ config/settings.py
@@ -181,3 +181,8 @@ SPECTACULAR_SETTINGS = {
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
