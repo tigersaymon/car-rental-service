37c926bb089a5fbe1224403714ee9aeeea13a55e
Vadim <vadimchubok@gmail.com>
2026-01-18 13:54:44 +0200

    chore(auth): added all endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import CreateUserView, ManageUserView
+from user.views import (
+    CreateUserView,
+    GoogleAuthExchangeCodeView,
+    GoogleAuthRedirectView,
+    ManageUserView,
+)
 
 
 urlpatterns = [
@@ -9,6 +14,8 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("auth/google/", GoogleAuthRedirectView.as_view(), name="google-auth-redirect"),
+    path("auth/google/exchange-code/", GoogleAuthExchangeCodeView.as_view(), name="google_exchange_code"),
 ]
 
 
